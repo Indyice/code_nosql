5c6281ff20bf0f8f5f6874f365533c16e2ebf04b
Tadsawan Waeohong <62160153@go.buu.ac.th>
2022-03-21 15:34:49 +0700

    Sidebar

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,14 +2,13 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title>Admin</title>
 
     <!-- Custom fonts for this template -->
     <link href="<?php echo base_url().'assets/template/startbootstrap-sb-admin-2-gh-pages'?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -23,4 +22,80 @@
 
 </head>
 
+
+
 <body>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url().'assets/template/startbootstrap-sb-admin-2-gh-pages'?>/index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Admin</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class=" nav-link" href="http://[::1]/code_nosql/">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                MANAGEMENT
+            </div>
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fa fa-bullseye"></i>
+                    <span>Activity</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <a class="collapse-item" href="buttons.html">Activity List</a>
+                        <a class="collapse-item" href="cards.html">Add Activity</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fa fa-trophy"></i>
+                    <span>Achievement</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <?php echo anchor('/Achievement/show_list', 'Achievement List', array('class'=> 'collapse-item'));?>
+                        <!-- <a class="collapse-item" href="">Achievement List</a> -->
+                        <a class="collapse-item" href="cards.html">Add Achievement</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
--- application/views/v_achievement_show_list.php
+++ application/views/v_achievement_show_list.php
@@ -1,78 +1,57 @@
-<?php
-	defined('BASEPATH') OR exit('No direct script access allowed');
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>Achievement</title>
-    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style.css" />
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <div class="container pt-4">
-        <div class="card">
-            <div class="card-header bg-primary text-white">
-                <h1>Achievement</h1>
+<div class="container pt-4">
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h1>Achievement</h1>
+        </div>
+        <div class="card-body">
+            <div>
+                <!-- <?php echo anchor('/Achievement/create', 'Create Achievement');?> -->
             </div>
-            <div class="card-body">
-                <div>
-                    <?php echo anchor('/Achievement/create', 'Create Achievement');?>
-                </div>
 
-                <div id="body">
-                    <?php
+            <div id="body">
+                <?php
           if ($ach) {
         ?>
-                    <table class="datatable">
-                        <thead>
-                            <tr>
-                                <th style="color: black;">Name</th>
-                                <th style="color: black;">Point</th>
-                                <th style="color: black;"> Acions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
+                <table class="datatable">
+                    <thead>
+                        <tr>
+                            <th style="color: black;">Name</th>
+                            <th style="color: black;">Point</th>
+                            <th style="color: black;"> Acions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
               $i = 0;
               foreach ($ach as $ams) {
                 $col_class = ($i % 2 == 0 ? 'odd_col' : 'even_col');
                 $i++;
               ?>
-                            <tr class="<?php echo $col_class; ?>">
-                                <td>
-                                    <?php echo $ams->ach_name; ?>
-                                </td>
-                                <td>
-                                    <?php echo $ams->ach_point; ?>
-                                </td>
-                                <td>
-                                    <?php echo anchor('/Achievement/update/' . $ams->_id, 'Update'); ?>
+                        <tr class="<?php echo $col_class; ?>">
+                            <td>
+                                <?php echo $ams->ach_name; ?>
+                            </td>
+                            <td>
+                                <?php echo $ams->ach_point; ?>
+                            </td>
+                            <td>
+                                <?php echo anchor('/Achievement/update/' . $ams->_id, 'Update'); ?>
 
-                                    <?php echo anchor('/Achievement/delete/' . $ams->_id, 'Delete', array('onclick' => "return confirm('Do you want delete this record')")); ?>
-                                </td>
-                            </tr>
-                            <?php
+                                <?php echo anchor('/Achievement/delete/' . $ams->_id, 'Delete', array('onclick' => "return confirm('Do you want delete this record')")); ?>
+                            </td>
+                        </tr>
+                        <?php
             }
             ?>
-                        </tbody>
-                    </table>
-                    <?php
+                    </tbody>
+                </table>
+                <?php
             } else {
                 echo '<div style="color:red;"><p>No Record Found!</p></div>';
             }
         ?>
-                </div>
             </div>
         </div>
-
     </div>
 
-</body>
-
-</html>
+</div>
