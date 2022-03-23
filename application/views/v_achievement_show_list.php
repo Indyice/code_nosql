f257f504bb2d8bb1d7511a38f815c2442aeb80a4
Tadsawan Waeohong <62160153@go.buu.ac.th>
2022-03-23 15:49:08 +0700

    status achievement

--- application/views/v_achievement_show_list.php
+++ application/views/v_achievement_show_list.php
@@ -48,7 +48,14 @@
                             <td><?php echo $i; ?></td>
                             <td><?php echo $ams->ach_name; ?></td>
                             <td><?php echo $ams->ach_point; ?></td>
-                            <td><?php echo $ams->ach_status; ?></td>
+                            <td>
+                                <div style="color: #1cc88a;">
+                                    <?php if($ams->ach_status == 1){echo 'Succeed';}?>
+                                </div>
+                                <div style="color: #f6c23e;">
+                                    <?php if($ams->ach_status == 0){echo 'Pending';} ?>
+                                </div>
+                            </td>
                             <td>
                                 <?php echo anchor('/Achievement/update/' . $ams->_id, 'Edit'); ?>
                                 <?php echo anchor('/Achievement/delete/' . $ams->_id, 'Delete', array('onclick' => "return confirm('Do you want delete this record')")); ?>
